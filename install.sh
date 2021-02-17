002aa3ce9f83c48491fa75c01cea407e5b9ec26a
Sharvil Ajmani <sharvilajmani92@gmail.com>
2021-02-17 19:20:08 +0530

    Second commit

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 
 sudo apt -y install nodejs
--- server_run.sh
+++ server_run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd backend/
 
 sudo systemctl start mongod.service
